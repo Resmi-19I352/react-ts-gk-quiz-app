a53e057055971cc2c01a1637452ae5898f0de366
Resmi-19I352 <19i352@psgtech.ac.in>
2021-05-10 13:30:27 +0530

    1 file modified

--- API.ts
+++ API.ts
@@ -18,7 +18,7 @@ export enum Difficulty {
 export type QuestionsState = Question & { answers: string[] };
 
 export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty): Promise<QuestionsState[]> => {
-  const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+  const endpoint = 'https://opentdb.com/api.php?amount=10&category=9'
   const data = await (await fetch(endpoint)).json();
   return data.results.map((question: Question) => ({
     ...question,
